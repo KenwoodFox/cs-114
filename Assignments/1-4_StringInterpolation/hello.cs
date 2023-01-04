da7167228d1bffe859444d7bd1e40d9499a51701
KenwoodFox <kenwood364@gmail.com>
2023-01-04 11:58:03 -0500

    Create stringInterpolation

---
+++ Assignments/1-4_StringInterpolation/hello.cs
@@ -0,0 +1,19 @@
+// Hello World
+
+// Alias
+using con = System.Console;
+
+namespace HelloWorld
+{
+    class Hello {
+        static void Main(string[] args)
+        {
+            string myString;
+
+            con.WriteLine("Welcome to the program. Please enter a string: ");
+            myString = con.ReadLine(); // Read a string from console
+
+            con.WriteLine($"Your string was \"{myString}\""); // Print that string back out
+        }
+    }
+}
