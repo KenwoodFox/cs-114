9707525454244492044d51343bbdd8d5c0c7f63e
KenwoodFox <kenwood364@gmail.com>
2023-02-07 15:50:38 -0500

    Boilerplate Special Project 1

---
+++ Assignments/SP1_ProcessLunchOrders/ProcessLunchOrders.cs
@@ -0,0 +1,14 @@
+// ProcessLunchOrders SP1
+
+// Alias
+using con = System.Console;
+
+namespace ProcessLunchOrders
+{
+    class ProcessLunchOrders {
+        static void Main(string[] args)
+        {
+            con.WriteLine("ProcessLunchOrders, SP1");
+        }
+    }
+}
