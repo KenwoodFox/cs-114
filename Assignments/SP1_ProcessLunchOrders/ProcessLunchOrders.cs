47c634581066542613dc7fd4bcb0e50fb48ca867
KenwoodFox <kenwood364@gmail.com>
2023-02-08 20:36:24 -0500

    This sucks

--- Assignments/SP1_ProcessLunchOrders/ProcessLunchOrders.cs
+++ Assignments/SP1_ProcessLunchOrders/ProcessLunchOrders.cs
@@ -1,14 +1,37 @@
 // ProcessLunchOrders SP1
+using Newtonsoft.Json;
 
 // Alias
 using con = System.Console;
 
 namespace ProcessLunchOrders
 {
-    class ProcessLunchOrders {
-        static void Main(string[] args)
-        {
-            con.WriteLine("ProcessLunchOrders, SP1");
-        }
+
+    static void Main(string[] args)
+    {
+        con.WriteLine("ProcessLunchOrders, SP1");
+
+        string fileName = "menu.json";
+        string jsonString = File.ReadAllText(fileName);
+        MenuItems menuItems =
+            JsonSerializer.Deserialize<Menuitems>(jsonString);
+
+        con.WriteLine(menuItems.items);
+    }
+
+    public class MenuItems
+    {
+        public MenuItem[] items { get; set; }
+    }
+
+    public class MenuItem
+    {
+        public string name { get; set; }
+
+        public double price { get; set; }
+
+        public string[] addOns { get; set; }
+
+        public double addOnPrice { get; set; }
     }
 }
