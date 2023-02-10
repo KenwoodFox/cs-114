65f2da8f6bd21d86c91bdfc1381b499b2eeb70d8
KenwoodFox <kenwood364@gmail.com>
2023-02-10 15:58:31 -0500

    Create python version because AAHH

---
+++ Assignments/ProcessLunchOrders/ui_form.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QRadioButton, QSizePolicy,
+    QWidget)
+
+class Ui_ProcessLunchOrders(object):
+    def setupUi(self, ProcessLunchOrders):
+        if not ProcessLunchOrders.objectName():
+            ProcessLunchOrders.setObjectName(u"ProcessLunchOrders")
+        ProcessLunchOrders.resize(800, 600)
+        self.groupBox = QGroupBox(ProcessLunchOrders)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(20, 20, 151, 171))
+        self.radioButton = QRadioButton(self.groupBox)
+        self.radioButton.setObjectName(u"radioButton")
+        self.radioButton.setGeometry(QRect(20, 40, 96, 23))
+        self.radioButton_2 = QRadioButton(self.groupBox)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+        self.radioButton_2.setGeometry(QRect(20, 80, 96, 23))
+        self.radioButton_3 = QRadioButton(self.groupBox)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+        self.radioButton_3.setGeometry(QRect(20, 120, 96, 23))
+        self.groupBox_2 = QGroupBox(ProcessLunchOrders)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setGeometry(QRect(190, 20, 251, 171))
+        self.radioButton_4 = QRadioButton(self.groupBox_2)
+        self.radioButton_4.setObjectName(u"radioButton_4")
+        self.radioButton_4.setGeometry(QRect(20, 40, 96, 23))
+        self.radioButton_5 = QRadioButton(self.groupBox_2)
+        self.radioButton_5.setObjectName(u"radioButton_5")
+        self.radioButton_5.setGeometry(QRect(20, 80, 96, 23))
+        self.radioButton_6 = QRadioButton(self.groupBox_2)
+        self.radioButton_6.setObjectName(u"radioButton_6")
+        self.radioButton_6.setGeometry(QRect(20, 120, 96, 23))
+        self.groupBox_3 = QGroupBox(ProcessLunchOrders)
+        self.groupBox_3.setObjectName(u"groupBox_3")
+        self.groupBox_3.setGeometry(QRect(20, 210, 151, 171))
+
+        self.retranslateUi(ProcessLunchOrders)
+
+        QMetaObject.connectSlotsByName(ProcessLunchOrders)
+    # setupUi
+
+    def retranslateUi(self, ProcessLunchOrders):
+        ProcessLunchOrders.setWindowTitle(QCoreApplication.translate("ProcessLunchOrders", u"ProcessLunchOrders", None))
+        self.groupBox.setTitle(QCoreApplication.translate("ProcessLunchOrders", u"GroupBox", None))
+        self.radioButton.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.radioButton_2.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.radioButton_3.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("ProcessLunchOrders", u"GroupBox", None))
+        self.radioButton_4.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.radioButton_5.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.radioButton_6.setText(QCoreApplication.translate("ProcessLunchOrders", u"RadioButton", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("ProcessLunchOrders", u"GroupBox", None))
+    # retranslateUi
+
