8a8acb7e04b88b7bf7b25f49af087f7edda3dcfa
KenwoodFox <kenwood364@gmail.com>
2023-03-01 08:51:13 -0800

    Add labels

--- Assignments/ProjectMaintenance/Form1.Designer.cs
+++ Assignments/ProjectMaintenance/Form1.Designer.cs
@@ -31,6 +31,9 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // textBox1
@@ -57,16 +60,47 @@
             textBox3.TabIndex = 2;
             textBox3.Tag = "Price";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(35, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Code";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(39, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Description";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(39, 168);
+            label3.Name = "label3";
+            label3.Size = new Size(33, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Price";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -76,5 +110,8 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
     }
 }
