938756adff6d493120893fe468ebe6ea4efaeddd
KenwoodFox <kenwood364@gmail.com>
2023-04-03 14:02:37 -0400

    Refactor templates

---
+++ Assignments/Lab8_Grocery/app.py
@@ -0,0 +1,53 @@
+#!/bin/python
+import sys
+
+
+from PyQt6 import uic
+from PyQt6.QtCore import QDate
+from PyQt6.QtWidgets import *
+
+
+class Grocery(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        # load ui file
+        uic.loadUi("app.ui", self)
+
+        # Connect controls
+        exitButton = self.findChild(QPushButton, "exitButton")
+        exitButton.clicked.connect(self.cleanExit)
+
+        addBtn = self.findChild(QPushButton, "addButton")
+        addBtn.clicked.connect(self.add)
+
+        removeButton = self.findChild(QPushButton, "removeButton")
+        removeButton.clicked.connect(self.remove)
+
+        clearBtn = self.findChild(QPushButton, "clearButton")
+        clearBtn.clicked.connect(self.clear)
+
+        self.listBox = self.findChild(QListWidget, "listWidget")
+
+        self.inputEdit = self.findChild(QLineEdit, "inputEdit")
+
+    def add(self):
+        self.listBox.insertItem(0, self.inputEdit.text())
+        self.inputEdit.setText("")
+
+    def remove(self):
+        for item in self.listBox.selectedItems():
+            self.listBox.removeItemWidget(item)
+
+    def clear(self):
+        self.listBox.clear()
+
+    def cleanExit(self):
+        sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Grocery()
+    widget.show()
+    sys.exit(app.exec())
--- Assignments/genProject.py
+++ Assignments/genProject.py
@@ -10,8 +10,6 @@ import os
 from glob import glob
 
 
-src = "0.0_Template"
-
 print(
     """
  ____            _           _   
@@ -29,8 +27,21 @@ print(
 A wizard for making a new challenge project from template\n --Joe\n\n"""
 )
 
-projName = str(input("Enter project Name   (Ex. HelloWorld ): "))
-projNum = str(input("Enter project Number (Ex. 2.19)       : "))
+projName = str(input("Enter project Name   (Ex. HelloWorld )             : "))
+projType = str(input("Enter project language[csharp] (Ex. csharp, python): "))
+projNum = str(input("Enter project Number (Ex. 2.19)                    : "))
+
+
+# Source type
+isCSharp = False
+if projType == "" or projType == "csharp":
+    src = "0.0_Template_csharp"
+    isCSharp = True
+elif projType == "python":
+    src = "0.0_Template_python"
+else:
+    print(f"Error, wrong type, didn't understand {projType}")
+    quit()
 
 # Destination path
 if len(projNum) != 0:
@@ -42,22 +53,26 @@ else:
 shutil.copytree(src, dest)
 
 # Rename cs file
-os.rename(f"{dest}/hello.cs", f"{dest}/{projName}.cs")
+if isCSharp:
+    os.rename(f"{dest}/hello.cs", f"{dest}/{projName}.cs")
 
 # All files
 newFiles = [y for x in os.walk(dest) for y in glob(os.path.join(x[0], "*"))]
 
 for file in newFiles:
-    print(f"Replacing text in {file}")
-    with open(file, "r") as curFile:
-        data = curFile.read()  # Contents of file
+    try:
+        print(f"Replacing text in {file}")
+        with open(file, "r") as curFile:
+            data = curFile.read()  # Contents of file
 
-        # Replacements
-        data = data.replace("~~name~~", projName)
-        data = data.replace("~~number~~", projNum)
+            # Replacements
+            data = data.replace("~~name~~", projName)
+            data = data.replace("~~number~~", projNum)
 
-    with open(file, "w") as curFile:
-        # Writeback out
-        curFile.write(data)
+        with open(file, "w") as curFile:
+            # Writeback out
+            curFile.write(data)
+    except IsADirectoryError:
+        pass
 
 print("Done.")
